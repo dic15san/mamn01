e1797e1ff6842ee82b6dd6580e4bdc4799d73d73
dic15san <dic15san@student.lu.se>
2018-04-15 14:42:43 +0200

    first commit

---
+++ app/src/main/java/se/example/stinaandreason/helloapp/MainActivity.java
@@ -0,0 +1,35 @@
+package se.example.stinaandreason.helloapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void compassButton(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, CompassActivity.class);
+      //  EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void accelerometersButton(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, AccelerometersActivity.class);
+       // EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
